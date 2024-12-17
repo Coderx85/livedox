0bd701b115cbfa74e42f748a0709687ec7ac20f4
Coderx85 <work.priyanshu085@gmail.com>
2024-12-17 21:15:08 +0530

    add Header and Loader components

---
+++ components/Header.tsx
@@ -0,0 +1,25 @@
+import { cn } from '@/lib/utils'
+import Image from 'next/image'
+import Link from 'next/link'
+
+const Header = ({ children, className }: HeaderProps) => {
+  return (
+    <div className={cn("header", className)}>
+      <Link href='/' className="md:flex-1">
+        <Image 
+          src="/assets/icons/logo-icon.svg"
+          alt="Logo"
+          width={32}
+          height={32}
+          className="mr-2 md:hidden"
+        />
+        <h2 className='text-primary text-2xl font-bold'>
+          LiveDox
+        </h2>
+      </Link>
+      {children}
+    </div>
+  )
+}
+
+export default Header
